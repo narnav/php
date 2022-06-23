8eb64fbd280552d5ea9007b05f97291323d01066
eyalg <narnavg@gmail.com>
2022-06-23 20:55:42 +0300

    sec

--- index.php
+++ index.php
@@ -2,12 +2,29 @@
 <html lang="en">
 <?php include 'head.html'?>
 <body>
-<?php include 'header.html'?>
+<?php
+include "sampDAL.php";
+ include 'header.html'?>
 <?php include 'nav.html'?>
 
 <div class="container mt-5">
-    <?php include 'left.html'?>
-    <?php include 'right.html'?>
+    <?php include 'left.html';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $desc = $_REQUEST['desc'];
+  $price = $_REQUEST['price'];
+  if (empty($desc)) {
+    echo "Name is empty";
+  } else {
+
+    $sql = "INSERT INTO `products` (`desc`,`price`)VALUES('$desc',$price);";
+    echo executeSQL($sql)?  "true": "false";
+
+  }
+}
+    
+    include 'right.html'?>
 </div>
 </div>
 <?php 
